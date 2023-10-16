982e284e8add90fd58e85d718dd6315530838bd9
Chris Ferrari <mpcn2002@gmail.com>
2023-10-16 21:50:15 +0200

    Aggiornato es.2

--- src/ferrari_chris/serie04/esercizio02/ClassEncapsulator.java
+++ src/ferrari_chris/serie04/esercizio02/ClassEncapsulator.java
@@ -1,94 +1,85 @@
 package ferrari_chris.serie04.esercizio02;
 
 import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.List;
 
 
 public class ClassEncapsulator {
-    public void encapsulate() throws ClassNotFoundException, IllegalAccessException {
-        Class<Target> targetClass = Target.class;
-        Field[] fields = targetClass.getDeclaredFields();
-        List<String> type = getType(fields);
-        List<String> name = getName(fields);
-        List<String> value = getValue(fields);
-        printClass(targetClass.getSimpleName(), type, name, value);
-
-    }
-    private List<String> getType(Field[] fields) throws ClassNotFoundException {
-        List<String> listOfTypes = new ArrayList<>();
-        for (int i = 0; i < fields.length; i++) {
-            listOfTypes.add(fields[i].getType().getSimpleName());
-        }
-        return listOfTypes;
-    }
-    private List<String> getName(Field[] fields) throws ClassNotFoundException {
-        List<String> listOfNames = new ArrayList<>();
-        for (int i = 0; i < fields.length; i++) {
-            listOfNames.add(fields[i].getName());
-        }
-        return listOfNames;
-    }
-    private List<String> getValue(Field[] fields) throws IllegalAccessException, ClassNotFoundException {
-        List<String> listOfValue = new ArrayList<>();
-        Target test = new Target();
-        for (int i = 0; i < fields.length; i++) {
-            fields[i].setAccessible(true);
-            if (fields[i].get(test) instanceof String){
-                listOfValue.add("\"" + fields[i].get(test) + "\"");
-                continue;
-            }
-            listOfValue.add(fields[i].get(test).toString());
-        }
-        return listOfValue;
-    }
-    private void printFields(List<String> typeList, List<String> nameList, List<String> valueList) throws ClassNotFoundException {
-
-        for (int i = 0; i < typeList.size(); i++) {
-            System.out.printf("\tprivate %s %s = %s;\n", typeList.get(i), nameList.get(i), valueList.get(i));
-        }
-        System.out.println();
-    }
-    private void printGetters(List<String> typeList, List<String> nameList){
-        String fieldName;
-        String rightFieldName;
-        for (int i = 0; i < nameList.size(); i++) {
-            fieldName = nameList.get(i);
-            rightFieldName = "get" + fieldName.substring(0, 1).toUpperCase() + fieldName.substring(1);
-            System.out.printf("\tpublic %s %s() {\n\t\treturn %s;\n\t}\n\n",
-                    typeList.get(i), rightFieldName, nameList.get(i));
-        }
-    }
-    private void printSetters(List<String> typeList, List<String> nameList){
-        String fieldName;
-        String rightFieldName;
-        for (int i = 0; i < nameList.size(); i++) {
-            fieldName = nameList.get(i);
-            rightFieldName = "set" + fieldName.substring(0, 1).toUpperCase() + fieldName.substring(1);
-            System.out.printf("\tpublic void %s(%s %s) {\n\t\tthis.%s = %s;\n\t}\n\n",
-                    rightFieldName, typeList.get(i), fieldName, fieldName, fieldName);
-        }
-    }
-    private void printClass(String target, List<String> typeList, List<String> nameList, List<String> valueList) throws ClassNotFoundException {
-        String rightClassName = target.substring(0, 1).toUpperCase() + target.substring(1);
-        System.out.printf("public class %s {\n", rightClassName);
-        printFields(typeList, nameList, valueList);
-        printGetters(typeList, nameList);
-        printSetters(typeList, nameList);
+    public static void encapsulate(Class<?> targetClass) throws IllegalAccessException {
+        System.out.println("public class " + targetClass.getSimpleName() + " {");
+        printFields(targetClass);
+        printGetters(targetClass);
+        printSetters(targetClass);
         System.out.println("}");
     }
-
-
-
-    public static void main(String[] args) {
-        ClassEncapsulator classEncapsulator = new ClassEncapsulator();
+    private static void printFields(Class<?> targetClass) {
         try {
-            classEncapsulator.encapsulate();
-
-        } catch (ClassNotFoundException c){
-            System.out.println("Classe non trovata.");
+            Field[] fields = targetClass.getDeclaredFields();
+            Target target = new Target();
+            for (Field f : fields) {
+                f.setAccessible(true);
+                String type = f.getType().getSimpleName();
+                String name = f.getName();
+                Object value = f.get(target);
+                int val = getModifierValue(f);
+                String modifier;
+                switch (val) {
+                    default -> {
+                        modifier = "private ";
+                        print(modifier, type, name, value);
+                    }
+                    //public static = 9
+                    //private static = 10
+                    case (9), (10) -> {
+                        modifier = "private static ";
+                        print(modifier, type, name, value);
+                    }
+                    //public + static + final = 25
+                    //private + static + final = 26
+                    case (25), (26) -> {
+                        modifier = "private static final ";
+                        print(modifier, type, name, value);
+                    }
+                }
+            }
         } catch (IllegalAccessException e){
-            System.out.println("Illegal access");
+            System.out.println("Error");
         }
+
+    }
+    private static int getModifierValue(Field field){
+        return field.getModifiers();
+    }
+    private static void print(String modifier, String type, String name, Object value){
+        if (value == null){
+            System.out.printf("\t%s%s %s;\n",modifier, type, name);
+        } else if (value instanceof String){
+            System.out.printf("\t%s%s %s = \"%s\";\n",modifier, type, name, value);
+        } else {
+            System.out.printf("\t%s%s %s = %s;\n",modifier, type, name, value);
+        }
+    }
+    private static void printGetters(Class<?> targetClass){
+        Field[] fields = targetClass.getDeclaredFields();
+        for (Field f : fields) {
+            String name = f.getName();
+            String getterName = f.getName().substring(0,1).toUpperCase() + f.getName().substring(1);
+            String type = f.getType().getSimpleName();
+            System.out.printf("\tpublic %s get%s() {\n", type, getterName);
+            System.out.printf("\t\treturn %s;\n\t}\n", name);
+        }
+    }
+    private static void printSetters(Class<?> targetClass){
+        Field[] fields = targetClass.getDeclaredFields();
+        for (Field f : fields) {
+            String name = f.getName();
+            String getterName = f.getName().substring(0,1).toUpperCase() + f.getName().substring(1);
+            String type = f.getType().getSimpleName();
+            System.out.printf("\tpublic %s set%s(%s %s) {\n", type, getterName, type, name);
+            System.out.printf("\t\tthis.%s = %s;\n\t}\n", name, name);
+        }
+    }
+
+    public static void main(String[] args) throws IllegalAccessException {
+        encapsulate(Target.class);
     }
 }
